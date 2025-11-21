8d851b29c79092597954b9142ef4ac063905f219
CavMCarolina <cavm.carolina@gmail.com>
2025-11-20 23:08:06 -0300

    tirando import desnecessario

--- components/Exercicios.js
+++ components/Exercicios.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import styles from '../style/style.js';
